d61048838dceb7c6ef5b4777e4bbecdb3d8f152f
chanchi- <chanchi0707@gmail.com>
2016-12-05 13:34:48 +0900

    enchantの概要、クマを動かす

--- public/js/index.js
+++ public/js/index.js
@@ -3,5 +3,32 @@
 enchant();
 
 window.onload = function () {
-	console.log("hello");
+
+	var core = new Core(320, 320);
+	core.preload("../img/chara1.png");
+	core.fps = 10;
+	core.onload = function () {
+
+		var bear = new Sprite(32, 32);
+		bear.image = core.assets["../img/chara1.png"];
+		bear.x = 0;
+		bear.y = 0;
+		bear.frame = 1;
+
+		//始まった時func
+		bear.addEventListener("enterframe", function () {
+			if (core.input.left) bear.x -= 5;
+			if (core.input.right) bear.x += 5;
+			if (core.input.up) bear.y -= 5;
+			if (core.input.down) bear.y += 5;
+		});
+
+		//何かをタッチしてfunc
+		bear.on("touchstart", function () {
+			core.rootScene.removeChild(bear);
+		});
+
+		core.rootScene.addChild(bear);
+	};
+	core.start();
 };
--- src/js/index.es6
+++ src/js/index.es6
@@ -1,5 +1,32 @@
 enchant();
 
-window.onload = () =>{
-	console.log("hello");
+window.onload = () => {
+
+	let core = new Core(320,320);
+	core.preload("../img/chara1.png");
+	core.fps = 10;
+	core.onload = () => {
+
+		let bear = new Sprite(32,32);
+		bear.image = core.assets["../img/chara1.png"];
+		bear.x = 0;
+		bear.y = 0;
+		bear.frame = 1;
+
+		//始まった時func
+		bear.addEventListener("enterframe",() => {
+			if (core.input.left) bear.x -= 5;
+			if (core.input.right) bear.x += 5;
+			if (core.input.up) bear.y -= 5;
+			if (core.input.down) bear.y += 5;
+		});
+
+		//何かをタッチしてfunc
+		bear.on("touchstart", () => {
+			core.rootScene.removeChild(bear);
+		})
+
+		core.rootScene.addChild(bear);
+	}
+	core.start();
 };
